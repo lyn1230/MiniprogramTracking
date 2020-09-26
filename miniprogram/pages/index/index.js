a5d9df93f29ae031c642aa242f3ded4d153d42e3
liyanan <825640357@qq.com>
2020-09-26 18:31:45 +0800

    tracking code

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var app = getApp();
+Page({
+    data: {},
+    onLoad: function () {
+    },
+    bindViewTap: function () {
+    },
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQ0EsSUFBTSxHQUFHLEdBQUcsTUFBTSxFQUFjLENBQUE7QUFFaEMsSUFBSSxDQUFDO0lBQ0gsSUFBSSxFQUFFLEVBQ0w7SUFDRCxNQUFNO0lBQ04sQ0FBQztJQUVELFdBQVc7SUFDWCxDQUFDO0NBQ0YsQ0FBQyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiLy/ojrflj5blupTnlKjlrp7kvothcHBcbmNvbnN0IGFwcCA9IGdldEFwcDxJQXBwT3B0aW9uPigpXG5cblBhZ2Uoe1xuICBkYXRhOiB7XG4gIH0sXG4gIG9uTG9hZCgpIHtcbiAgfSxcbiAgLy/kuovku7blpITnkIblh73mlbBcbiAgYmluZFZpZXdUYXAoKSB7XG4gIH0sXG59KVxuIl19
---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,12 @@
+//获取应用实例app
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+  },
+  onLoad() {
+  },
+  //事件处理函数
+  bindViewTap() {
+  },
+})
